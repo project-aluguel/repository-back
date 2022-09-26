30e15e31a122518eb7876ddce15526e7284a6462
Diego Leopoldino Silva <T807888@tokiomarine.com.br>
2022-09-26 09:56:45 -0300

    dependencia

---
+++ renthouse/src/main/java/com/renthouse/renthouse/mensageiro/RabbitMQConfig.java
@@ -0,0 +1,13 @@
+package com.renthouse.renthouse.mensageiro;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.naming.Binding;
+@Configuration
+public class RabbitMQConfig {
+
+//    @Bean
+//    public Queue queue(){
+//        return new Queue("");
+//    }
+}
