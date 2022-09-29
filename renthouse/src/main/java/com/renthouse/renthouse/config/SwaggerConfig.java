90e579cc904e38e890428e7d7c2bb3147b5ba0bb
Diego Leopoldino Silva <T807888@tokiomarine.com.br>
2022-09-28 21:19:54 -0300

    xml

---
+++ renthouse/src/main/java/com/renthouse/renthouse/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package com.renthouse.renthouse.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+}
